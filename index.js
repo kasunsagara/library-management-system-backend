d00e795c60d141084030f5ef75e93c57a3c5e570
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2024-12-01 20:33:31 +0530

    1

---
+++ index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const mongoUrl = process.env.MONGO_DB_URI;
+
+mongoose.connect(mongoUrl,{});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+    console.log("Database connected");
+});
+
+app.use(bodyParser.json());
+
+app.use(
+
+    (req, res, next) => {
+
+        const token = req.header("Authorization")?.replace("Bearer ", "");
+        console.log(token);
+
+        if(token != null) {
+            jwt.verify(token, process.env.SECRET, (error, decoded) => {
+
+                if(!error) {
+                    req.user = decoded;
+                }
+            });
+        }
+        next();
+    }
+);
+                
+app.use("/api/users", userRouter);
+
+app.listen(
+    5000,
+     () => {
+        console.log("Server is running on port 5000");
+    }
+);
---
+++ models/user.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isBlocked: {
+        type: Boolean,
+        default: false
+    },
+    type: {
+        type: String,
+        default: "customer"
+    },
+    profilePicture: {
+        type: String,
+        default: "https://t3.ftcdn.net/jpg/05/53/79/60/360_F_553796090_XHrE6R9jwmBJUMo9HKl41hyHJ5gqt9oz.jpg"
+    }
+});
+
+const User = mongoose.model("users", userSchema);
+
+export default User;
+    

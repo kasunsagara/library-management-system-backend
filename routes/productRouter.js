697aef5d2cecd4f7ae35a1c7f159b6c748916ee2
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2024-12-29 15:57:54 +0530

    create product delete function

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createProduct, getProducts } from "../controllers/productController.js";
+import { createProduct, getProducts, deleteProduct } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/", createProduct);
 productRouter.get("/", getProducts);
+productRouter.delete("/:productId", deleteProduct);
 
 export default productRouter;

7b0195edd1e271567dc5784a003af54ba6ebe474
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2025-02-10 21:56:31 +0530

    2

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -12,7 +12,8 @@ export async function createOrder(req, res) {
     }
 
     try {
-        const latestOrder = await Order.find().sort({ date: -1 }).limit(1);
+        const latestOrder = await Order.find().sort({ orderId: -1 }).limit(1);
+        console.log(latestOrder);
 
         let orderId;
 
@@ -23,7 +24,7 @@ export async function createOrder(req, res) {
 
             const numberString = currentOrderId.replace("CBC", "");
 
-            const number = parseInt(numberString)
+            const number = parseInt(numberString);
 
             const newNumber = (number + 1).toString().padStart(4, "0");
 
@@ -47,8 +48,8 @@ export async function createOrder(req, res) {
 
             newProductArray[i] = {
                 name: product.productName,
-                price: product.price,
-                quantity: newOrderData.orderedItems[i].quantity,
+                price: product.lastPrice,
+                quantity: newOrderData.orderedItems[i].qty,
                 image: product.images[0]
             }
         }
@@ -61,10 +62,11 @@ export async function createOrder(req, res) {
 
         const order = new Order(newOrderData);
 
-        await order.save();
+        const savedOrder = await order.save();
 
         res.json({
-            message: "Order created"
+            message: "Order created",
+            order : savedOrder
         });
     } catch (error) {
         res.status(500).json({
@@ -74,15 +76,73 @@ export async function createOrder(req, res) {
 }
 
 export async function getOrders(req, res) {
-
+  
     try {
-        const orders = await Order.find({ email: req.user.email });
+      if (isCustomer(req)) {
+      const orders = await Order.find({ email: req.user.email });
+  
+      res.json(orders);
+      return;
+      }else if(isAdmin(req)){
+        const orders = await Order.find({});
 
         res.json(orders);
-
+        return;
+      }else{
+        res.json({
+          message: "Please login to view orders"
+        })
+      }
     } catch (error) {
         res.status(500).json({
-            message: error.message
+            message: error.message,
         });
+      }
     }
-}
+
+    export async function getQuote(req, res) {
+  
+        try {
+          const newOrderData = req.body;
+          const newProductArray = [];
+          let total = 0;
+          let labeledTotal = 0;
+          console.log(req.body)
+          for (let i = 0; i < newOrderData.orderedItems.length; i++) {
+            const product = await Product.findOne({
+              productId: newOrderData.orderedItems[i].productId,
+            });
+            if (product == null) {
+              res.json({
+                message:
+                  "Product with id " +
+                  newOrderData.orderedItems[i].productId +
+                  " not found",
+              });
+              return;
+            }
+            labeledTotal += product.price * newOrderData.orderedItems[i].qty;
+            total += product.lastPrice * newOrderData.orderedItems[i].qty;
+            newProductArray[i] = {
+              name: product.productName,
+              price: product.lastPrice,
+              labeledPrice: product.price,
+              quantity: newOrderData.orderedItems[i].qty,
+              image: product.images[0],
+            };
+          }
+          console.log(newProductArray);
+          newOrderData.orderedItems = newProductArray;
+          newOrderData.total = total;
+          res.json({
+            orderedItems: newProductArray,
+            total: total,
+            labeledTotal: labeledTotal,
+          });
+
+        } catch (error) {
+            res.status(500).json({
+                message: error.message,
+            });
+          }
+        }

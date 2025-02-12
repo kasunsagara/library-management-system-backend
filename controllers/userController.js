8d44984b180ef90043f0855a56daf85dfdea99a9
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2025-02-12 05:33:02 +0530

    6

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
@@ -107,4 +108,68 @@ export function isCustomer(req) {
     }
 
     return true;
-}  
+} 
+
+export async function googleLogin(req,res){
+    console.log(req.body)
+    const token = req.body.token
+    //'https://www.googleapis.com/oauth2/v3/userinfo'
+    try{
+      const response = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo',{
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      const email = response.data.email
+      //check if user exists
+      const usersList = await User.find({email: email})
+      if(usersList.length >0){
+        const user = usersList[0]
+        const token = jwt.sign({
+          email : user.email,
+          firstName : user.firstName,
+          lastName : user.lastName,
+          isBlocked : user.isBlocked,
+          type : user.type,
+          profilePicture : user.profilePicture
+        } , process.env.SECRET)
+        
+        res.json({
+          message: "User logged in",
+          token: token,
+          user : {
+            firstName : user.firstName,
+            lastName : user.lastName,
+            type : user.type,
+            profilePicture : user.profilePicture,
+            email : user.email
+          }
+        })
+      }else{
+        //create new user
+        const newUserData = {
+          email: email,
+          firstName: response.data.given_name,
+          lastName: response.data.family_name,
+          type: "customer",
+          password: "ffffff",
+          profilePicture: response.data.picture
+        }
+        const user = new User(newUserData)
+        user.save().then(()=>{
+          res.json({
+            message: "User created"
+          })
+        }).catch((error)=>{
+          res.json({      
+            message: "User not created"
+          })
+        })
+      }
+    }catch(e){
+      res.json({
+        message: "Google login failed"
+      })
+    }
+  }
+  

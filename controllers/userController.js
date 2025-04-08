9087414bcaca5a060b9699d381f51e4573b92b2e
Kasun Sagara <156450587+kasunsagara@users.noreply.github.com>
2025-04-08 09:44:13 +0530

    small change

--- controllers/userController.js
+++ controllers/userController.js
@@ -124,3 +124,15 @@ export async function getUser(req, res) {
         res.status(500).json({ message: "Error fetching user data.", error: error.message });
     }
 }
+
+export async function getUsers(req, res) {
+    try {
+      const users = await User.find(); 
+      res.json(users);
+    } catch (err) {
+      res.status(500).json({
+        message: "Error retrieving users",
+        error: err.message
+      });
+    }
+  }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getUser, createUser, loginUser } from '../controllers/userController.js';
+import { getUser, getUsers, createUser, loginUser } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.get("/", getUser);
+userRouter.get("/all", getUsers);
 userRouter.post("/", createUser);
 userRouter.post("/login", loginUser);
 
